880d3bf3476fde9a1fff92c35c4b1b902f361fd7
Peter Halliday <pghalliday@gmail.com>
2014-12-03 16:37:21 +0100

    use VUNDLE_INSTALL_DIR env var

--- converge.sh
+++ converge.sh
@@ -3,7 +3,7 @@ BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 VUNDLE_INSTALL_DIR=~/.vim/bundle/Vundle.vim
 if ! [ -d "$VUNDLE_INSTALL_DIR" ]
 then
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	git clone https://github.com/gmarik/Vundle.vim.git $VUNDLE_INSTALL_DIR
 fi
 ln -fs $BASEDIR/.vimrc ~/
 vim +PluginInstall +qall
