1d1c0c8cc314553d74a64720c5002cd8acfa8705
Halliday, Peter <phalliday@libertyglobal.com>
2015-01-14 17:09:15 +0100

    refactoring bashrc and bash_aliases stuff and adding tmux configuration

--- converge.sh
+++ converge.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
+
+rm -rf ~/.dotfiles
+mkdir -p ~/.dotfiles
+
+# enable sourcing additional bashrc config files from ~/.dotfiles/bashrc.d
+if ! grep -q .dotfiles/bashrc.d ~/.bashrc; then
+  cat << 'EOF' >> ~/.bashrc
+
+for file in $HOME/.dotfiles/bashrc.d/*
+do . $file
+done
+EOF
+fi
+cp -r $BASEDIR/bashrc.d ~/.dotfiles/
+
+# enable sourcing additional bash_aliases config files from ~/.dotfiles/bash_aliases.d
+if ! grep -q .dotfiles/bash_aliases.d ~/.bash_aliases; then
+  cat << 'EOF' >> ~/.bash_aliases
+
+for file in $HOME/.dotfiles/bash_aliases.d/*
+do . $file
+done
+EOF
+fi
+cp -r $BASEDIR/bash_aliases.d ~/.dotfiles/
+
+# base16 theme configuration (also see bashrc.d/base16 and .vimrc)
+BASE16_SHELL_INSTALL_DIR=~/.config/base16-shell
+if ! [ -d "$BASE16_SHELL_INSTALL_DIR" ]
+then
+	git clone https://github.com/chriskempson/base16-shell.git $BASE16_SHELL_INSTALL_DIR
+fi
+
+# tmux configuration (also see bash_aliases.d/tmux)
+cp -f $BASEDIR/.tmux.conf ~/
+
+# VIM configuration
 VUNDLE_INSTALL_DIR=~/.vim/bundle/Vundle.vim
 if ! [ -d "$VUNDLE_INSTALL_DIR" ]
 then
 	git clone https://github.com/gmarik/Vundle.vim.git $VUNDLE_INSTALL_DIR
 fi
-BASE16_SHELL_INSTALL_DIR=~/.config/base16-shell
-if ! [ -d "$BASE16_SHELL_INSTALL_DIR" ]
-then
-	git clone https://github.com/chriskempson/base16-shell.git $BASE16_SHELL_INSTALL_DIR
-  cat << 'EOF' >> ~/.bashrc
-
-# Base16 Shell
-BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
-[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
-EOF
-fi
-ln -fs $BASEDIR/.vimrc ~/
+cp -f $BASEDIR/.vimrc ~/
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swap
 mkdir -p ~/.vim/undo
