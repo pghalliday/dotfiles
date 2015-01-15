e8ad61d7ceec497c8419dee83ab74deeb9ba4ad1
Peter Halliday <pghalliday@gmail.com>
2015-01-15 19:45:06 +0100

    added simple dotfiles tmux configuration

--- bash_aliases.d/tmuxomatic.sh
+++ bash_aliases.d/tmuxomatic.sh
@@ -1,1 +1,2 @@
 alias tmux-haf="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/helios-application-framework"
+alias tmux-dotfiles="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/dotfiles"
