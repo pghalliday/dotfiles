ee81cb60e5a5646d16c85353c331282ae33b189f
Halliday, Peter <phalliday@libertyglobal.com>
2015-01-16 15:13:45 +0100

    refactoring tmux sessions and setting terminal title to tmux session name

--- bash_aliases.d/tmuxomatic.sh
+++ bash_aliases.d/tmuxomatic.sh
@@ -1,2 +1,3 @@
 alias tmux-haf="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/helios-application-framework"
 alias tmux-dotfiles="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/dotfiles"
+alias tmux-scratchpad="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/scratchpad"
