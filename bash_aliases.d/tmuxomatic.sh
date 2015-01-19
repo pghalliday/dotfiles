1123e51a92e737a50ead915dd63ad06a4b4e7974
Peter Halliday <pghalliday@gmail.com>
2015-01-19 02:08:10 +0100

    added a taaks project session

--- bash_aliases.d/tmuxomatic.sh
+++ bash_aliases.d/tmuxomatic.sh
@@ -1,3 +1,4 @@
 alias tmux-haf="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/helios-application-framework"
+alias tmux-tasks="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/tasks"
 alias tmux-dotfiles="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/dotfiles"
 alias tmux-scratchpad="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/scratchpad"
