a5f6f23ea02ce93d7c9591734aeb1988a3c9216c
Halliday, Peter <phalliday@libertyglobal.com>
2015-01-21 14:58:59 +0100

    added tmux session for helios development infrastructure

--- bash_aliases.d/tmuxomatic.sh
+++ bash_aliases.d/tmuxomatic.sh
@@ -1,4 +1,5 @@
 alias tmux-helios-application-framework="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/helios-application-framework"
+alias tmux-helios-development-infrastructure="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/helios-development-infrastructure"
 alias tmux-tasks="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/tasks"
 alias tmux-grunt-mocha-test="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/grunt-mocha-test"
 alias tmux-jira-time-series="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/jira-time-series"
