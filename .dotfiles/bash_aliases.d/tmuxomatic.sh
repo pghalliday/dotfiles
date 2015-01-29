67ea89122e1061f7ac6da8a33c320acab1989d2f
Halliday, Peter <phalliday@libertyglobal.com>
2015-01-29 12:36:05 +0100

    rename jira project

--- .dotfiles/bash_aliases.d/tmuxomatic.sh
+++ .dotfiles/bash_aliases.d/tmuxomatic.sh
@@ -2,7 +2,7 @@ alias tmux-helios-application-framework="TERM=xterm-256color tmuxomatic ~/.dotfi
 alias tmux-helios-development-infrastructure="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/helios-development-infrastructure"
 alias tmux-tasks="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/tasks"
 alias tmux-grunt-mocha-test="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/grunt-mocha-test"
-alias tmux-jira-time-series="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/jira-time-series"
+alias tmux-jira-project-analysis="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/jira-project-analysis"
 alias tmux-dotfiles="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/dotfiles"
 alias tmux-spikes="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/spikes"
 alias tmux-vim="TERM=xterm-256color tmuxomatic ~/.dotfiles/tmuxomatic/vim"
