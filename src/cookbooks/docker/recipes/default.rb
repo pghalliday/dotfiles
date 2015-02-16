eed5be1893478986f3ac0c5ebdfff78d1a4bbe23
Peter Halliday <pghalliday@gmail.com>
2015-02-16 11:33:48 +0100

    install fig with docker

--- src/cookbooks/docker/recipes/default.rb
+++ src/cookbooks/docker/recipes/default.rb
@@ -7,3 +7,12 @@ apt_repository 'docker' do
   key '36A1D7869245C8950F966E92D8576A8BA88D21E9'
 end
 package 'lxc-docker'
+
+package 'python'
+package 'python-pip'
+fig_installed = Mixlib::ShellOut.new('which fig')
+fig_installed.run_command
+bash 'install fig' do
+  code 'pip install -U fig'
+  only_if {fig_installed.error?}
+end
