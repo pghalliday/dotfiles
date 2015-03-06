9014b2bd3747b2a037ef780f1f2b79a17c568423
Peter Halliday <pghalliday@gmail.com>
2015-03-06 08:50:39 +0000

    Built from commit 22a8669c28e83db8dbc50425f8bb63acb2bd4c93

--- cookbooks/bash/resources/alias.rb
+++ cookbooks/bash/resources/alias.rb
@@ -6,3 +6,4 @@ attribute :command, kind_of: String
 attribute :user, kind_of: String
 attribute :subshell, kind_of: [TrueClass, FalseClass], default: false
 attribute :arg_offset, kind_of: Integer, default: 0
+attribute :force_system_command, kind_of: [TrueClass, FalseClass], default: false
--- cookbooks/tmux/providers/default.rb
+++ cookbooks/tmux/providers/default.rb
@@ -7,9 +7,12 @@ use_inline_resources
 action :add do
   home = ::Dir.home(new_resource.user)
   group = ::Etc.getpwnam(new_resource.user).gid
+  # create the bash alias but prevent recursion
+  # with the force_system_command option
   bash_alias 'tmux' do
     command 'tmux -2'
     user new_resource.user
+    force_system_command true
   end
   cookbook_file "#{home}/.tmux.conf" do
     cookbook 'tmux'
