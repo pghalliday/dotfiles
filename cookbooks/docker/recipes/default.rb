2e0f574cb305eda3ebe8f62c0d27544057b2b94e
Peter Halliday <pghalliday@gmail.com>
2015-03-18 15:39:03 +0100

    replace fig with docker-compose

--- cookbooks/docker/recipes/default.rb
+++ cookbooks/docker/recipes/default.rb
@@ -8,11 +8,14 @@ apt_repository 'docker' do
 end
 package 'lxc-docker'
 
-package 'python'
-package 'python-pip'
-fig_installed = Mixlib::ShellOut.new('which fig')
-fig_installed.run_command
-bash 'install fig' do
-  code 'pip install -U fig'
-  only_if {fig_installed.error?}
+remote_file '/usr/local/bin/docker-compose' do
+  source 'https://github.com/docker/compose/releases/download/1.1.0/docker-compose-Linux-x86_64'
+  checksum '2e2d87d3e6dc795821598cc71790a79e360068dbdee6a3cf6c55b6bfd856fd83'
+  mode 0755
+end
+
+remote_file '/etc/bash_completion.d/docker-compose' do
+  source 'https://raw.githubusercontent.com/docker/compose/1.1.0/contrib/completion/bash/docker-compose'
+  checksum 'd8bf9f150faa46253ff49c41af734ac42496f2a69442578aa39f337f61181587'
+  mode 0644
 end
