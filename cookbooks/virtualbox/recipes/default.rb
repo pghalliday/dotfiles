b68aabe77b2339ea76ada6ce89a047cd85d6820a
Peter Halliday <pghalliday@gmail.com>
2015-03-21 11:01:35 +0000

    Built from commit c91b9575fa0516b5fa1c441e784321aa0f55c026

--- cookbooks/virtualbox/recipes/default.rb
+++ cookbooks/virtualbox/recipes/default.rb
@@ -1,3 +1,4 @@
+extension_pack = ::File.join(Chef::Config[:file_cache_path], 'virtualbox.vbox-extpack')
 apt_repository 'virtualbox' do
   uri 'http://download.virtualbox.org/virtualbox/debian'
   distribution node['lsb']['codename']
@@ -7,4 +8,14 @@ end
 package 'dkms'
 package 'virtualbox-4.3' do
   timeout 1800
+  notifies :run, 'bash [install VirtualBox extension pack]', :immediately
+end
+bash 'install VirtualBox extension pack' do
+  code <<-EOH
+    version=$(VBoxManage --version)
+    IFS='r' read -a versions <<< "${version}"
+    wget -O #{extension_pack} http://download.virtualbox.org/virtualbox/#{versions[0]}/Oracle_VM_VirtualBox_Extension_Pack-#{versions[0]}-#{versions[1]}.vbox-extpack
+    VBoxManage extpack install --replace #{extension_pack}
+  EOH
+  action :nothing
 end
