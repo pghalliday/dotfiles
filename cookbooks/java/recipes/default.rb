9f28bd5394fd937e4f0ed7f3e7912420bdbc3bbf
Peter Halliday <pghalliday@gmail.com>
2015-03-30 21:44:24 +0200

    fix java ca certs

--- cookbooks/java/recipes/default.rb
+++ cookbooks/java/recipes/default.rb
@@ -1,1 +1,9 @@
 package 'openjdk-8-jre-headless'
+
+# workaround for broken java cacerts
+bash 'install java cacerts' do
+  code <<-EOH
+  /var/lib/dpkg/info/ca-certificates-java.postinst configure
+  EOH
+  not_if { ::File.exist?('/etc/ssl/certs/java/cacerts') }
+end
