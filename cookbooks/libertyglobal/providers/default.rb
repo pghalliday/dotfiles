6efd0a5aaa444f0ee32b23b2a2018c3cceb24640
Peter Halliday <pghalliday@gmail.com>
2015-04-09 16:19:52 +0000

    Built from commit 626e3533e670400388f75a588da296d527732f74

--- cookbooks/libertyglobal/providers/default.rb
+++ cookbooks/libertyglobal/providers/default.rb
@@ -5,38 +5,28 @@ end
 use_inline_resources
 
 action :add do
-  home = ::Dir.home(new_resource.user)
-  group = ::Etc.getpwnam(new_resource.user).gid
-  development = "#{home}/development"
-  directory development do
-    owner new_resource.user
-    group group
-    mode 0755
-  end
   %w{
     vagrant-office
   }.each do |name|
-    project "development/#{name}" do
+    project name do
       user new_resource.user
-      cookbook 'libertyglobal'
-      tmuxomatic "tmuxomatic/#{name}"
       repository "git@gitlab.upc.biz:phalliday/#{name}.git"
       email 'phalliday@libertyglobal.com'
     end
   end
   bash_alias 'office-up' do
     user new_resource.user
-    command 'cd ~/development/vagrant-office/;vagrant up'
+    command 'cd ~/projects/vagrant-office/;vagrant up'
     subshell true
   end
   bash_alias 'office-halt' do
     user new_resource.user
-    command 'cd ~/development/vagrant-office/;vagrant halt'
+    command 'cd ~/projects/vagrant-office/;vagrant halt'
     subshell true
   end
   bash_alias 'office-destroy' do
     user new_resource.user
-    command 'cd ~/development/vagrant-office/;vagrant destroy'
+    command 'cd ~/projects/vagrant-office/;vagrant destroy'
     subshell true
   end
 end
