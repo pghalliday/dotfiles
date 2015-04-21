c824e62c9ad6569b675326ed58257e9b201436dc
Peter Halliday <pghalliday@gmail.com>
2015-04-21 11:54:32 +0200

    fix version of vim markdown plugin

--- cookbooks/vim-markdown/providers/default.rb
+++ cookbooks/vim-markdown/providers/default.rb
@@ -7,6 +7,7 @@ use_inline_resources
 action :add do
   vim_plugin 'plasticboy/vim-markdown' do
     user new_resource.user
+    revision '0da5ba9d719b818909d85bacdad2ce5d860c9e94'
   end
   vim_config 'vim-markdown' do
     cookbook 'vim-markdown'
--- cookbooks/vim/resources/plugin.rb
+++ cookbooks/vim/resources/plugin.rb
@@ -2,4 +2,5 @@ actions :add
 default_action :add
 
 attribute :github, name_attribute: true, kind_of: String
+attribute :revision, kind_of: String
 attribute :user, kind_of: String
