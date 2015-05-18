3aa2f192d89439a491495008831e751b81b65a95
Peter Halliday <pghalliday@gmail.com>
2015-05-18 16:02:06 +0000

    Built from commit 8bf732cab27368c5a2b6db2ab6dc962c92f77f55

--- converge.sh
+++ converge.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
-set -e
-
-sudo chef-client -z -j ~/.dotfiles/attributes.json -r workstation
+#!/bin/bash -e
+DIR=$(cd "$(dirname "$0")"; pwd)
+ATTRIBUTES=$HOME/.dotfiles/attributes.json
+sudo -i -- sh -c "cd $DIR && chef-client -z -j $ATTRIBUTES -r workstation"
--- cookbooks/android-sdk/providers/avd.rb
+++ cookbooks/android-sdk/providers/avd.rb
@@ -6,12 +6,14 @@ use_inline_resources
 
 action :add do
   home = ::Dir.home(new_resource.user)
+  group = ::Etc.getpwnam(new_resource.user).gid
   install_dir = node['android-sdk']['install_dir']
   bash "create android avd #{new_resource.name}" do
     code <<-EOH
       #{install_dir}/current/tools/android create avd -n "#{new_resource.name}" -t "#{new_resource.target}" --skin "#{new_resource.skin}" --abi "#{new_resource.abi}"
       EOH
     user new_resource.user
+    group group
     not_if { ::File.exist?(::File.join(home, '.android', 'avd', 'default.ini')) }
   end
 end
