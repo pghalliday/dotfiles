8bf732cab27368c5a2b6db2ab6dc962c92f77f55
Peter Halliday <pghalliday@gmail.com>
2015-05-18 17:51:45 +0200

    run in a root shell so as not to pollute the user's home directory with files and folders owned by root

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -e
 
 sudo rm -rf ./vendor/
 mkdir ./vendor/
 chef exec berks vendor ./vendor/cookbooks/
-cat > ./vendor/converge.sh << EOH
-#!/bin/bash
-
-set -e
-
-sudo chef-client -z -j ~/.dotfiles/attributes.json -r workstation
+cat > ./vendor/converge.sh << 'EOH'
+#!/bin/bash -e
+DIR=$(cd "$(dirname "$0")"; pwd)
+ATTRIBUTES=$HOME/.dotfiles/attributes.json
+sudo -i -- sh -c "cd $DIR && chef-client -z -j $ATTRIBUTES -r workstation"
 EOH
 chmod +x ./vendor/converge.sh
