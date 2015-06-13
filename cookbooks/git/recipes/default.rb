1bff8d2d7aa06e6c8c6036e6769400f245576e21
Peter Halliday <pghalliday@gmail.com>
2015-06-13 10:37:02 +0200

    install and configure git

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -1,8 +1,6 @@
-apt_repository 'git-core' do
-  uri 'http://ppa.launchpad.net/git-core/ppa/ubuntu'
-  distribution node['lsb']['codename']
-  components ['main']
-  keyserver 'keyserver.ubuntu.com'
-  key 'E1DD270288B4E6030699E45FA1715D88E1DF1F24'
+case node['platform']
+when 'ubuntu'
+  include_recipe 'git::ubuntu'
+when 'mac_os_x'
+  include_recipe 'git::mac_os_x'
 end
-package 'git'
