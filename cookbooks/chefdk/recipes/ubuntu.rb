bdf6a221c2397feaa449158e081725c0702a15e8
Peter Halliday <pghalliday@gmail.com>
2015-07-10 13:52:44 +0200

    update chefdk to version 0.6.2

--- cookbooks/chefdk/recipes/ubuntu.rb
+++ cookbooks/chefdk/recipes/ubuntu.rb
@@ -1,7 +1,7 @@
 chefdk_deb = "#{Chef::Config[:file_cache_path]}/chefdk.deb"
 remote_file chefdk_deb do
-  source 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.4.0-1_amd64.deb'
-  checksum 'e135c0719fc80fc7b95560e90839103167308a45d4927cf8da9c22bdc385cc7d'
+  source 'https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.6.2-1_amd64.deb'
+  checksum 'e6990a3821590b999d09b707ff124fa891d44849cd63fd4f6f9db73bf5cb23f9'
 end
 package 'chefdk' do
   source chefdk_deb
